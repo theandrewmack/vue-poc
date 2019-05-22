c686abb952e75952e59426a090253900fedda57e
Andrew Mack <amack1221@gmail.com>
2019-05-21 21:26:01 -0500

    added axios and example REST call

--- src/foobar/App.vue
+++ src/foobar/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="This is the FooBar Page"/>
+    <ul v-if="posts && posts.length">
+      <li v-for="post of posts" v-bind:key="post.id">
+        <p>
+          <strong>{{post.title}}</strong>
+        </p>
+        <p>{{post.body}}</p>
+      </li>
+    </ul>
+
+    <ul v-if="errors && errors.length">
+      <li v-for="{error, index} of errors" v-bind:key="index">{{error.message}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'index',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      posts: [],
+      errors: []
+    };
+  },
+  created() {
+    axios
+      .get("http://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        this.posts = response.data;
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+/* #app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
